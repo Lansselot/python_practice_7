d808cbd439551215cc22837b0f59f87a3d7afb06
lansselot <pozharov2003@gmail.com>
2024-03-17 23:14:38 +0200

    add empty functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_text_from_console():
+    pass
+
+
+def read_text_from_file(filename):
+    pass
+
+
+def read_text_from_file_pandas(filename):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def output_text_to_console(text):
+    pass
+
+
+def write_text_to_file(filename, text):
+    pass
+
+
+def write_text_to_file_pandas(filename, text):
+    pass
